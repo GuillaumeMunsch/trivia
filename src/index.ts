84f9181da9eb9e19770768abb1d143462d829252
Guillaume Munsch <guillaume.munsch@ovrsea.com>
2023-03-15 16:03:37 +0100

    Basic refactos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+type Category = "Pop" | "Science" | "Sports" | "Rock";
+
 export class Game {
   private players: Array<string> = [];
   private places: Array<number> = [];
@@ -16,16 +18,12 @@ export class Game {
       this.popQuestions.push("Pop Question " + i);
       this.scienceQuestions.push("Science Question " + i);
       this.sportsQuestions.push("Sports Question " + i);
-      this.rockQuestions.push(this.createRockQuestion(i));
+      this.rockQuestions.push("Rock Question " + i);
     }
     console.log(`Pop questions: ${this.popQuestions.length}`);
     console.log(`Science questions: ${this.scienceQuestions.length}`);
     console.log(`Sports questions: ${this.sportsQuestions.length}`);
-    console.log(`Rock questions: ${this.rockQuestions.length}`);   
-  }
-
-  private createRockQuestion(index: number): string {
-    return "Rock Question " + index;
+    console.log(`Rock questions: ${this.rockQuestions.length}`);
   }
 
   public add(name: string) {
@@ -42,63 +40,67 @@ export class Game {
   }
 
   public roll(roll: number) {
-      console.log(`Player ${this.currentPlayer} rolled ${roll}`);
-      if (this.inPenaltyBox[this.currentPlayer]) {
-        console.log(`While in penalityBox`);
+    console.log(`Player ${this.currentPlayer} rolled ${roll}`);
+    if (this.inPenaltyBox[this.currentPlayer]) {
+      console.log(`While in penalityBox`);
 
-        if (roll % 2 != 0) {
-          console.log(`He rolled an odd number`);
-          this.isGettingOutOfPenaltyBox = true;
-          if (this.isGettingOutOfPenaltyBox === true) console.log(`He might get out of penality box`);
-
-          this.places[this.currentPlayer] = this.places[this.currentPlayer] + roll;
-
-          if (this.places[this.currentPlayer] > 11) {
-            this.places[this.currentPlayer] = this.places[this.currentPlayer] - 12;
-          }
-          console.log(`He moves to place ${this.places[this.currentPlayer]}`);
-
-          this.askQuestion();
-        } else {
-          console.log(`He won't get out of penality box`);
-          this.isGettingOutOfPenaltyBox = false;
-        }
-      } else {
-        console.log(`While not being in penalityBox`);
+      if (roll % 2 != 0) {
+        console.log(`He rolled an odd number`);
+        this.isGettingOutOfPenaltyBox = true;
+        if (this.isGettingOutOfPenaltyBox === true) console.log(`He might get out of penality box`);
 
         this.places[this.currentPlayer] = this.places[this.currentPlayer] + roll;
+
         if (this.places[this.currentPlayer] > 11) {
           this.places[this.currentPlayer] = this.places[this.currentPlayer] - 12;
         }
         console.log(`He moves to place ${this.places[this.currentPlayer]}`);
 
         this.askQuestion();
+      } else {
+        console.log(`He won't get out of penality box`);
+        this.isGettingOutOfPenaltyBox = false;
       }
+    } else {
+      console.log(`While not being in penalityBox`);
+
+      this.places[this.currentPlayer] = this.places[this.currentPlayer] + roll;
+      if (this.places[this.currentPlayer] > 11) {
+        this.places[this.currentPlayer] = this.places[this.currentPlayer] - 12;
+      }
+      console.log(`He moves to place ${this.places[this.currentPlayer]}`);
+
+      this.askQuestion();
+    }
   }
 
   private askQuestion(): void {
-    if (this.currentCategory() == "Pop") console.log(`Asking question: ${this.popQuestions.shift()}`);
-    if (this.currentCategory() == "Science") console.log(`Asking question: ${this.scienceQuestions.shift()}`);
-    if (this.currentCategory() == "Sports") console.log(`Asking question: ${this.sportsQuestions.shift()}`);
-    if (this.currentCategory() == "Rock") console.log(`Asking question: ${this.rockQuestions.shift()}`);
+    const currentCategory = this.currentCategory();
+    switch (currentCategory) {
+      case "Pop":
+        console.log(`Asking question: ${this.popQuestions.shift()}`);
+        break;
+      case "Science":
+        console.log(`Asking question: ${this.scienceQuestions.shift()}`);
+        break;
+      case "Sports":
+        console.log(`Asking question: ${this.sportsQuestions.shift()}`);
+        break;
+      case "Rock":
+        console.log(`Asking question: ${this.rockQuestions.shift()}`);
+        break;
+    }
   }
 
-  private currentCategory(): string {
-    if (this.places[this.currentPlayer] == 0) return "Pop";
-    if (this.places[this.currentPlayer] == 4) return "Pop";
-    if (this.places[this.currentPlayer] == 8) return "Pop";
-    if (this.places[this.currentPlayer] == 1) return "Science";
-    if (this.places[this.currentPlayer] == 5) return "Science";
-    if (this.places[this.currentPlayer] == 9) return "Science";
-    if (this.places[this.currentPlayer] == 2) return "Sports";
-    if (this.places[this.currentPlayer] == 6) return "Sports";
-    if (this.places[this.currentPlayer] == 10) return "Sports";
-    return "Rock";
+  private currentCategory(): Category {
+    const categories: Category[] = ["Pop", "Science", "Sports", "Rock"];
+    const currentCategory = categories[this.places[this.currentPlayer] % 4];
+    return currentCategory;
   }
 
   private didPlayerWin(): boolean {
     console.log(`Player ${this.currentPlayer} did${this.purses[this.currentPlayer] == 6 ? "" : " not"} win`);
-    
+
     return this.purses[this.currentPlayer] == 6;
   }
 
@@ -112,7 +114,7 @@ export class Game {
 
     if (this.currentPlayer == this.players.length) this.currentPlayer = 0;
     console.log(`The current player is now ${this.currentPlayer}`);
-    
+
     return false;
   }
 
